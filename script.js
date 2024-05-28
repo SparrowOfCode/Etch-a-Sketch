c1f8b2f9abdc1fefb3a70a9ab72c8fb226393796
SparrowOfCode <163876500+SparrowOfCode@users.noreply.github.com>
2024-05-28 13:18:02 -0400

    Edit div grid and add mouseover event

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const mainDiv = document.querySelector('.container');
+const mainDiv = document.querySelector('.innerContainer');
+const gridSize = 16;
 
-for (let i = 0; i < 256; i++) {
-    const multipleDivs = document.createElement('div');
-    multipleDivs.className = "sketchPad";
-    mainDiv.appendChild(multipleDivs);
+for (let i = 0; i < gridSize; i++) {
+    const column = document.createElement('div');
+    column.className = "sketchPadColumn";
+    for (let j = 0; j < gridSize; j++) {
+        const row = document.createElement('div');
+        row.className = "sketchPadRow";
+        row.innerText = (i * gridSize) + j;
+        row.style.color = "white";
+        column.appendChild(row);
+    }
+    mainDiv.appendChild(column);
+}
+
+const elements = document.querySelectorAll('.sketchPadRow');
+
+for (i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('mouseover', function () {
+        this.style.backgroundColor = "black";
+        this.style.color = "black";
+    })
 }
